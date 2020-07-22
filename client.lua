b4948f78d6f1595945cc9921587578c996803937
iyadh2010 <54715760+iyadh2010@users.noreply.github.com>
2020-07-22 21:02:49 +0100

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,25 @@
+ESX = nil
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+local a = false
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(10)
+		local playerPed = GetPlayerPed(-1)
+		local vehicle = GetVehiclePedIsIn(playerPed, false)
+		local speed = (GetEntitySpeed(vehicle))
+		local InVeh = IsPedInVehicle(playerPed, vehicle, false)
+		if IsControlJustPressed(1, 47) and InVeh then
+			if a == false then
+				SetEntityMaxSpeed(vehicle, speed)
+				a = true
+				ESX.ShowNotification('Speed limiter ~g~Activated~g~')
+			else
+				SetEntityMaxSpeed(vehicle, 9999.9)
+				a = false
+				ESX.ShowNotification('Speed limiter ~g~Desactivated~g~')
+			end
+		end
+		
+	end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'bodacious'
+game 'gta5'
+author 'IYOOO'
+description 'speedlimiter'
+version '1.0.0'
+
+shared_scripts {
+    '@es_extended/locale.lua'
+}
+client_script 'client.lua'
+
