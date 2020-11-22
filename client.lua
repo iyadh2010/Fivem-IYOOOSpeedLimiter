dfaef6cdb64ded0b3a300c40ebfcb776c1fc68ee
iyadh2010 <54715760+iyadh2010@users.noreply.github.com>
2020-11-22 23:24:30 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,25 +1,31 @@
 ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-local a = false
+local IsLimiteractive = false
 
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(10)
-		local playerPed = GetPlayerPed(-1)
-		local vehicle = GetVehiclePedIsIn(playerPed, false)
-		local speed = (GetEntitySpeed(vehicle))
-		local InVeh = IsPedInVehicle(playerPed, vehicle, false)
-		if IsControlJustPressed(1, 47) and InVeh and speed > 0 then
-			if a == false then
-				SetEntityMaxSpeed(vehicle, speed)
-				a = true
-				ESX.ShowNotification('Speed limiter ~g~Activated~g~')
-			else
-				SetEntityMaxSpeed(vehicle, 9999.9)
-				a = false
-				ESX.ShowNotification('Speed limiter ~g~Desactivated~g~')
-			end
+		
+		if IsControlJustPressed(1, 47) then
+			setSpeedLimit()
 		end
 		
 	end
 end)
+
+function setSpeedLimit()
+	local playerPed = GetPlayerPed(-1)
+	local vehicle = GetVehiclePedIsIn(playerPed, false)
+	local speed = (GetEntitySpeed(vehicle))
+	local InVeh = IsPedInVehicle(playerPed, vehicle, false)
+	if InVeh and speed > 0 then
+		IsLimiteractive = not IsLimiteractive
+		if not IsLimiteractive then
+			SetEntityMaxSpeed(vehicle, speed)
+			ESX.ShowNotification('Speed limiter ~g~Activated~g~')
+		else
+			SetEntityMaxSpeed(vehicle, 250.0)
+			ESX.ShowNotification('Speed limiter ~g~Desactivated~g~')
+		end
+	end
+end
